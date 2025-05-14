8212402f17048085a57677ee8c7042707c0dbed5
Hela HAFSIA <he.hafsia@gmail.com>
2025-05-14 15:13:00 +0200

    Model Trainer

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -15,3 +15,18 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path #	Chemin vers le répertoire racine où les fichiers seront sauvegardés.
+    data_path: Path # Chemin vers le dataset à utiliser pour l'entraînement
+    model_ckpt: Path #Chemin vers le checkpoint du modèle pré-entrainé.
+    num_train_epochs: int
+    warmup_steps: int
+    per_device_train_batch_size: int
+    weight_decay: float
+    logging_steps: int
+    evaluation_strategy: str
+    eval_steps: int
+    save_steps: float
+    gradient_accumulation_steps: int
